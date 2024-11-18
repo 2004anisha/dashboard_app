95a49481fcd368bd0c14dec04dfcda8cacae77ec
2004anisha <2004anisha@gmail.com>
2024-11-18 13:52:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import EvaluationDashboard from './components/EvaluationDashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <EvaluationDashboard />
+    </div>
+  );
+}
+
+export default App;
+
